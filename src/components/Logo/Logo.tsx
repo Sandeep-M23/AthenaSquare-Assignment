ab6d222b71d858ed22629a2a7df01a1b86fbd94f
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2023-01-24 14:10:28 +0530

    Made Logo Component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,16 @@
+import type { FC } from "react";
+import classes from '../../styles/Logo.module.css';
+
+const Logo: FC = () => {
+  return (
+    <div className={classes.logo}>
+      <img
+        src="https://global-uploads.webflow.com/62efc7cb58ad153bfb146988/62efcbe40b52a66c04de66ce_Frame%2011.png"
+        alt="logo"
+        className={classes.Image}
+      />
+    </div>
+  );
+};
+
+export default Logo;
