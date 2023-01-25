d83c709b581c5529b1f5f9e9661148e04221bc31
Sandeep.M <sandeep.m24.rathnam@gmail.com>
2023-01-25 13:14:52 +0530

    Optimized The Code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,89 +1,97 @@
 import React, { useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
-import classes from '../styles/Home.module.css';
+import classes from "../styles/Home.module.scss";
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
-  const [video, setVideo] = useState(1);
-  const [data,setData] = useState();
-    const [ref1, inView1] = useInView(
-        {
-            threshold: 0.3,
-        }
-    );
-    const [ref2, inView2] = useInView(
-        {
-            threshold: 0.3,
-        }
-    );
-    const [ref3, inView3] = useInView(
-        {
-            threshold: 0.3,
-        }
-    );
-    useEffect(() => {
-        if (inView1) {
-            setVideo(1)
-        }
-        if (inView2) {
-            setVideo(2)
-        }
-        if (inView3) {
-            setVideo(3)
-        }
-    }, [inView1, inView2, inView3])
+interface Data {
+  heading: string;
+  subHeading: string;
+  description: string;
+}
 
-    const getData = async () => {
-      const res = await fetch(`https://mocki.io/v1/ee762599-31ae-4a3d-a6c7-d596525945e1`)
-      const data = await res.json();
-    setData(data.texts);
-    console.log(data)
+const Home: NextPage<{}> = () => {
+  const [activeVideo, setActiveVideo] = useState(1);
+  const [data, setData] = useState<Data[]>();
+  const [ref1, inView1] = useInView({ threshold: 0.3 });
+  const [ref2, inView2] = useInView({ threshold: 0.3 });
+  const [ref3, inView3] = useInView({ threshold: 0.3 });
+
+  useEffect(() => {
+    if (inView1) {
+      setActiveVideo(1);
     }
+    if (inView2) {
+      setActiveVideo(2);
+    }
+    if (inView3) {
+      setActiveVideo(3);
+    }
+  }, [inView1, inView2, inView3]);
 
-    useEffect(() => {
-      getData()
-    },[])
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://mocki.io/v1/ee762599-31ae-4a3d-a6c7-d596525945e1`
+      );
+      const data = await res.json();
+      setData(data.texts);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className={classes.Home}>
+    <div className={classes.home}>
       <div className={classes.introSection}>
-        <h1>Double the hires,half the effort</h1>
-        <div className={classes.para}>
-        <p>Open converstations and nuture relationships and be the first choice when your ideal</p>
-        <p>candidate is ready to explore.</p></div>
-        <button>View Kula Outreach</button>
+        <h1 className={classes.introTitle}>Double the hires,half the effort</h1>
+        <div className={classes.introText}>
+          <p>
+            Open converstations and nuture relationships and be the first choice
+            when your ideal
+          </p>
+          <p>candidate is ready to explore.</p>
+        </div>
+        <button className={classes.viewButton}>View Kula Outreach</button>
       </div>
       <div className={classes.mainSection}>
-        <div className={classes.text}>
+        <div className={classes.textContainer}>
           {data &&
-            data.map((sec, idx) => {
+            data.map((d, index) => {
               return (
                 <section
-                  key={idx}
-                  ref={idx == 0 ? ref1 : idx == 1 ? ref2 : ref3}
+                  key={index}
+                  ref={index == 0 ? ref1 : index == 1 ? ref2 : ref3}
                 >
-                  <div className={classes.heading}>
-                    <h2>{sec.heading}</h2>
-                    <h1>{sec.subHeading}</h1>
-                    <p>{sec.description.slice(0,150)}</p>
+                  <div className={classes.sectionHeading}>
+                    <h2 className={classes.sectionTitle}>{d.heading}</h2>
+                    <h1 className={classes.sectionSubTitle}>{d.subHeading}</h1>
+                    <p className={classes.sectionDescription}>
+                      {d.description.slice(0, 150)}
+                    </p>
                   </div>
-                  <div className={classes.mobileVideo}>
+                  <div className={classes.mobileVideoContainer}>
                     <video
                       autoPlay
                       loop
                       muted
-                      src={`/assets/Videos/vid${idx + 1}.mp4`}
+                      className={classes.mobileVideo}
+                      src={`/assets/Videos/vid${index + 1}.mp4`}
                     />
                   </div>
                 </section>
               );
             })}
         </div>
-        <div className={classes.video}>
-          <video autoPlay loop muted src={`/assets/Videos/vid${video}.mp4`} />
+        <div className={classes.videoContainer}>
+          <video
+            autoPlay
+            loop
+            muted
+            className={classes.mainVideo}
+            src={`/assets/Videos/vid${activeVideo}.mp4`}
+          />
         </div>
       </div>
-      <div className={classes.extraSection}>
-      </div>
+      <div className={classes.extraSection}></div>
     </div>
   );
 };
